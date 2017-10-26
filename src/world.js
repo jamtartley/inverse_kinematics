c6a2fddfa69d363bcab81bb15c5096c101778cbd
Sam Hartley <samhartley92@gmail.com>
2017-10-26 23:07:46 +0100

    Add update/draw loop for canvas resizing

--- src/world.js
+++ src/world.js
@@ -1,16 +1,44 @@
 import Chain from './chain';
 import Segment from './segment';
 
-var canvas = document.getElementById('canvas');
-var chain = new Chain(5);
-var context;
+function init() {
+    const frameRate = 60;
+    const millisBetweenUpdate = 1 / frameRate;
 
-if (canvas && canvas.getContext) {
+    canvas.addEventListener("mousemove", mouseMove, false);
+    setInterval(update, millisBetweenUpdate);
+    resize();
+
     context = canvas.getContext("2d");
+}
+
+function mouseMove(e) {
+    currentMousePos.x = e.layerX;
+    currentMousePos.y = e.layerY;
+}
+
+function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+}
+
+function update() {
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    resize();
     context.fillStyle = "rgb(50, 50, 50)";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     chain.draw(context);
+}
+
+var canvas = document.getElementById('canvas');
+var chain = new Chain(5);
+var context;
+var currentMousePos = {
+    x: 0,
+    y: 0
+};
+
+if (canvas && canvas.getContext) {
+    init();
 }
